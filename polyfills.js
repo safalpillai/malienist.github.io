4c8ab650871852b51ffa8bd65f2410b6abb4bf28
Safal Pillai <safalpillai@gmail.com>
2019-03-18 13:29:12 +0530

    Add files via upload

--- polyfills.js
+++ polyfills.js
@@ -3453,7 +3453,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! F:\Angular\sunlite\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! F:\angular\sunlite\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
